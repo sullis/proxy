e6a29b11d09b3441586d6b1367c768417aa82971
Michael Bryzek <mbryzek@alum.mit.edu>
2016-10-16 08:42:42 -0400

    Update tests

--- test/lib/ResolvedTokenSpec.scala
+++ test/lib/ResolvedTokenSpec.scala
@@ -1,6 +1,6 @@
 package lib
 
-import io.flow.common.v0.models.{Environment, Role}
+import io.flow.common.v0.models.{Environment, OrganizationReference, Role, UserReference}
 import io.flow.organization.v0.models.OrganizationAuthorization
 import java.util.UUID
 
@@ -10,64 +10,86 @@ import org.scalatestplus.play._
 import play.api.test._
 import play.api.test.Helpers._
 
+import io.flow.common.v0.models.{Environment, Role}
+import io.flow.token.v0.models._
+
 class FlowAuthSpec extends PlaySpec with OneServerPerSuite {
 
   private[this] val requestId = UUID.randomUUID.toString
 
-  "FlowAuthData.token" in {
-    val token = ResolvedToken(userId = "5")
-    FlowAuthData.fromToken(requestId, token) must be(
-      FlowAuthData(
+  "ResolvedToken.token" in {
+    ResolvedToken.fromUser("rid", userId = "5") must be(
+      ResolvedToken(
         requestId = requestId,
         userId = "5",
-        organization = None,
+        organizationId = None,
+        partnerId = None,
         role = None,
         environment = None
       )
     )
   }
 
-  "FlowAuthData.org" in {
-    val token = ResolvedToken(userId = "5", organizationId = Some("tst"))
-    FlowAuthData.org(requestId, token, OrganizationAuthorization(role = Role.Member, environment = Environment.Production)) must be(
-      FlowAuthData(
-        requestId = requestId,
+  "ResolvedToken.org" in {
+    val token = OrganizationTokenReference(
+      id = "0",
+      organization = OrganizationReference(id = "tst"),
+      environment = Environment.Production,
+      user = UserReference("5")
+    )
+
+    ResolvedToken.fromToken("rid", token) must be(
+      ResolvedToken(
+        requestId = "0",
         userId = "5",
-        organization = Some("tst"),
-        role = Some("member"),
+        organizationId = Some("tst"),
+        role = None,
         environment = Some("production")
       )
     )
   }
 
   "map contains only values" in {
-    val d = FlowAuthData(
+    val d = ResolvedToken(
       requestId = requestId,
       userId = "5",
-      organization = None,
+      organizationId = None,
+      partnerId = None,
       role = None,
       environment = None
     )
     d.toMap must be(Map("request_id" -> requestId, "user_id" -> "5", "created_at" -> dateTime.print(d.createdAt)))
 
-    val d2 = FlowAuthData(
+    val d2 = ResolvedToken(
       requestId = requestId,
       userId = "5",
-      organization = Some("flow"),
+      organizationId = Some("flow"),
+      partnerId = None,
       role = None,
       environment = None
     )
     d2.toMap must be(Map("request_id" -> requestId, "user_id" -> "5", "created_at" -> dateTime.print(d2.createdAt), "organization" -> "flow"))
 
-    val d3 = FlowAuthData(
+    val d3 = ResolvedToken(
       requestId = requestId,
       userId = "5",
-      organization = Some("flow"),
+      organizationId = None,
+      partnerId = Some("flow"),
+      role = None,
+      environment = None
+    )
+    d3.toMap must be(Map("request_id" -> requestId, "user_id" -> "5", "created_at" -> dateTime.print(d2.createdAt), "partner" -> "flow"))
+    
+    val d4 = ResolvedToken(
+      requestId = requestId,
+      userId = "5",
+      organizationId = Some("flow"),
+      partnerId = None,
       role = Some("member"),
       environment = None
     )
 
-    d3.toMap must be(
+    d4.toMap must be(
       Map(
         "request_id" -> requestId,
         "user_id" -> "5",
@@ -77,15 +99,16 @@ class FlowAuthSpec extends PlaySpec with OneServerPerSuite {
       )
     )
 
-    val d4 = FlowAuthData(
+    val d5 = ResolvedToken(
       requestId = requestId,
       userId = "5",
-      organization = Some("flow"),
+      organizationId = Some("flow"),
+      partnerId = None,
       role = Some("member"),
       environment = Some("production")
     )
 
-    d4.toMap must be(
+    d5.toMap must be(
       Map(
         "request_id" -> requestId,
         "user_id" -> "5",

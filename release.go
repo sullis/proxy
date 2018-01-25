0f882fa1572ca8014141fbedd9a6bfb305b9b780
Michael Bryzek <mike@flow.io>
2018-01-25 17:14:00 -0500

    Update release script

--- release.go
+++ release.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"github.com/flowcommerce/tools/executor"
+	"github.com/flowcommerce/tools/scala_library"
 )
 
 func main() {
-	executor := executor.Create("proxy")
-	executor = executor.Add("dev tag")
-	executor = executor.Add("dev build_docker_image")
-	executor.Run()
+	scala_library.Run()
 }

fe33763005fedbe02be48d1f053dbc0032f1742a
Michael Bryzek <mike@flow.io>
2018-02-08 13:56:39 -0500

    Cleanup import, comment

--- app/handlers/JsonpHandler.scala
+++ app/handlers/JsonpHandler.scala
@@ -2,7 +2,6 @@ package handlers
 
 import javax.inject.{Inject, Singleton}
 
-import io.apibuilder.validation.FormData
 import lib.{ProxyRequest, ResolvedToken, Route, Server}
 import play.api.libs.ws.WSClient
 import play.api.mvc.Result
@@ -12,7 +11,7 @@ import scala.concurrent.{ExecutionContext, Future}
 /**
   * Converts query parameters for a JSON P GET request
   * into a JSON body, then delegates processing to the
-  * application json handler
+  * application url form encoded handler
   */
 @Singleton
 class JsonpHandler @Inject() (

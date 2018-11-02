b7fd9d60553ba4d9af268ab66b73ba4180ac0e32
Paolo Lim <paolo@flow.io>
2018-11-02 10:28:44 -0400

    Send extra server config metadata to rollbar (#346)

--- app/lib/ErrorHandler.scala
+++ app/lib/ErrorHandler.scala
@@ -10,18 +10,27 @@ import javax.inject.{Inject, Singleton}
 
 @Singleton
 class ErrorHandler @Inject() (
-  logger: RollbarLogger
+  logger: RollbarLogger,
+  proxyConfigFetcher: ProxyConfigFetcher
 ) extends HttpErrorHandler with Errors {
+
+  lazy val index: Index = proxyConfigFetcher.current()
 
   def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
     val errorId = generateErrorId()
+    val operation = index.resolve(Method(request.method), request.uri)
+
     logger.
       withKeyValue("error_id", errorId).
       withKeyValue("request_method", request.method.toString).
       withKeyValue("request_uri", request.uri).
       withKeyValue("http_status_code", statusCode).
       withKeyValue("message", message).
+      withKeyValue("route_path", operation.map(_.route.path)).
+      withKeyValue("route_server_name", operation.map(_.server.name)).
+      withKeyValue("route_server_host", operation.map(_.server.host)).
       warn("Client error")
+
     Future.successful(
       Status(statusCode)(clientErrors(Seq(s"Invalid request (err #$errorId)")))
     )
@@ -29,10 +38,15 @@ class ErrorHandler @Inject() (
 
   def onServerError(request: RequestHeader, ex: Throwable): Future[Result] = {
     val errorId = generateErrorId()
+    val operation = index.resolve(Method(request.method), request.uri)
+
     logger.
       withKeyValue("error_id", errorId).
       withKeyValue("request_method", request.method.toString).
       withKeyValue("request_uri", request.uri).
+      withKeyValue("route_path", operation.map(_.route.path)).
+      withKeyValue("route_server_name", operation.map(_.server.name)).
+      withKeyValue("route_server_host", operation.map(_.server.host)).
       error("FlowError", ex)
 
     Future.successful(

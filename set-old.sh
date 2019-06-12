728f3cd18fe6817fed85e08ff4ab3e9201ccc815
Michael Bryzek <mike@flow.io>
2019-06-12 14:24:40 -0400

    Add temporary scripts to set env

---
+++ set-old.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+AWS_PROFILE=pci dev env set --env production --keyval APIBUILDER_SERVICE_URIS=https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-proxy/latest/api.service.json,https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-internal-proxy/latest/api-internal.service.json,https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-partner-proxy/latest/api-partner.service.json,https://s3.amazonaws.com/io.flow.aws-s3-public/util/apibuilder/flow/shopify-external.latest.json
+
+AWS_PROFILE=pci dev env list --env production | grep APIB
+

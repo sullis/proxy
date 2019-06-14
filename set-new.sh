60e17170562378a42ca887c80975f0371a617fef
Michael Bryzek <mike@flow.io>
2019-06-14 13:09:57 -0400

    Update environment

--- set-new.sh
+++ set-new.sh
@@ -2,4 +2,3 @@
 
 AWS_PROFILE=pci dev env set --env production --keyval APIBUILDER_SERVICE_URIS=https://s3.amazonaws.com/io.flow.aws-s3-public/util/lib-apibuilder/specs.zip
 AWS_PROFILE=pci dev env list --env production | grep APIB
-
--- set-old.sh
+++ set-old.sh
@@ -4,5 +4,9 @@ AWS_PROFILE=pci dev env set --env production --keyval APIBUILDER_SERVICE_URIS=ht
 
 AWS_PROFILE=pci dev env set --env development --keyval APIBUILDER_SERVICE_URIS=https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-proxy/latest/api.service.json:https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-internal-proxy/latest/api-internal.service.json:https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-partner-proxy/latest/api-partner.service.json:https://s3.amazonaws.com/io.flow.aws-s3-public/util/apibuilder/flow/shopify-external.latest.json
 
+AWS_PROFILE=pci dev env set --env production --keyval PROXY_CONFIG_URIS=https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-proxy/latest/production.config.yml:https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-internal-proxy/latest/production.config.yml:https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-partner-proxy/latest/production.config.yml
+
+AWS_PROFILE=pci dev env set --env development --keyval PROXY_CONFIG_URIS=https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-proxy/latest/production.config.yml:https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-internal-proxy/latest/production.config.yml:https://s3.amazonaws.com/io.flow.aws-s3-public/util/api-partner-proxy/latest/production.config.yml
+
 AWS_PROFILE=pci dev env list --env production | grep APIB
 

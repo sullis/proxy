18e169a81dd4e5201824ebf4f93f03aa35b96caf
Paolo Lim <paolo@flow.io>
2019-10-29 12:56:30 -0400

    Add util script to login to all proxy instances (#420)

---
+++ docker_login.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+# Simple script to make sure you are logged in on docker
+# on the relevant nodes.
+#
+# Important: Make sure you have these environment variables set:
+#   `DOCKER_USERNAME`: Your username for hub.docker.com
+#   `DOCKER_PASSWORD`: Your password for the username account on hub.docker.com
+#
+# Usage:
+#  ./docker-login.rb
+#
+
+if ENV['DOCKER_USERNAME'].nil? || ENV['DOCKER_PASSWORD'].nil?
+  raise "DOCKER_USERNAME and DOCKER_PASSWORD must be valid environment variables"
+end
+
+dir = File.dirname(__FILE__)
+nodes_file = File.join(dir, 'nodes')
+
+if !File.exists?(nodes_file)
+  puts "ERROR: Nodes configuration file[%s] not found" % nodes_file
+  exit(1)
+end
+
+nodes = IO.readlines(nodes_file).map(&:strip).select { |l| !l.empty? }
+
+nodes.each do |ip|
+  cmd = <<TXT
+ssh #{ip} "docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD"
+TXT
+  puts cmd
+  `#{cmd}`
+end
